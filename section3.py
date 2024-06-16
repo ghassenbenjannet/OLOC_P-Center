91cfadcfe1073874eee3c7bad31e673401930eba
ghassenbenjannet <ghassen.b.j.15@gmail.com>
2024-06-16 18:34:28 +0200

    modified:   section3.py

--- section3.py
+++ section3.py
@@ -23,19 +23,50 @@ def min_distance_method(points, p):
     
     return np.array(antennes)
 
+def improved_min_distance_method(points, p):
+    antennes = []
+
+    first_index = np.random.choice(range(len(points)))
+    antennes.append(points[first_index])
+    
+    for _ in range(p - 1):
+        max_min_distance = -1
+        next_antenne = None
+        
+        for point in points:
+            if any(np.array_equal(point, antenne) for antenne in antennes):
+                continue
+            
+            min_distance = np.min([np.linalg.norm(point - antenne) for antenne in antennes])
+            
+            if min_distance > max_min_distance:
+                max_min_distance = min_distance
+                next_antenne = point
+                
+        antennes.append(next_antenne)
+    
+    return np.array(antennes)
+
 def plot_solution(points, antennes):
     plt.figure(figsize=(10, 6))
     plt.scatter(points[:, 0], points[:, 1], color='blue', label='Villes')
     plt.scatter(antennes[:, 0], antennes[:, 1], color='red', label='Antennes')
     
+    max_distance = 0
     for point in points:
         distances = np.linalg.norm(point - antennes, axis=1)
         closest_antenne_index = np.argmin(distances)
         closest_antenne = antennes[closest_antenne_index]
         plt.plot([point[0], closest_antenne[0]], [point[1], closest_antenne[1]], 'red', linestyle='-', linewidth=0.5)
+        
+        # Mise à jour de la distance maximale
+        max_distance = max(max_distance, np.min(distances))
     
     plt.legend()
     plt.grid(True)
+    plt.title(f"Solution avec p = {len(antennes)} antennes\nRayon maximal des distances (Z) = {max_distance:.2f}")
+    plt.xlabel('Coordonnée X')
+    plt.ylabel('Coordonnée Y')
     plt.show()
 
 def read_uflp_file(file_name):
@@ -45,10 +76,10 @@ def read_uflp_file(file_name):
     return np.column_stack((tabX, tabY)) 
 
 # Main
-nom_fichier = "inst_0.flp" 
+nom_fichier = "inst_1000.flp" 
 points = read_uflp_file(nom_fichier)
-p = 16000
+p = 15  
 
-antennes = min_distance_method(points, p)
+antennes = improved_min_distance_method(points, p)
 
 plot_solution(points, antennes)
